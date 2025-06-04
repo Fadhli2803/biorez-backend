662a4c2f833228a4c65b4d84d22692336c6281a9
Fadhli <mfadhli2803@gmail.com>
2025-06-04 18:54:15 +0700

    menambahkan properti category pada shop.js dan scripts start pada package.json

--- src/shop.js
+++ src/shop.js
@@ -3,27 +3,24 @@ const { nanoid } = require("nanoid");
 const shopItems = [
     {
         "id": nanoid(16),
+        "category": "kendaraan",
         "imageUrl": "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png",
         "itemName": "Sepatu Second",
         "price": 50,
     },
     {
         "id": nanoid(16),
+        "category": "elektronik",
         "imageUrl": "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png",
         "itemName": "TV Bekas",
         "price": 100,
     },
     {
         "id": nanoid(16),
+        "category": "pakaian",
         "imageUrl": "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png",
         "itemName": "Hoodie Second",
         "price": 75,
-    },
-    {
-        "id": nanoid(16),
-        "imageUrl": "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png",
-        "itemName": "Kaos",
-        "price": 25,
     },
 ];
 
